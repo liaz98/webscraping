04bf263cccc304b3b74962231eccc93cb8a29cdf
Azizabonu Israilova <azibonu19@gmail.com>
2021-12-01 14:55:33 +0500

    Added

--- onlinecourse/settings.py
+++ onlinecourse/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-STATIC_ROOT = BASE_DIR / 'statics'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # STATICFILES_DIRS = [
 #     BASE_DIR / 'statics'
